49372f8f63b1c0ae3f1ec49ff6ea0f807e92c4c6
yzhxxzxy <yzhxxzxy@163.com>
2021-06-09 16:19:35 +0800

    test

---
+++ cn/bak/nav.js
@@ -0,0 +1,10 @@
+document.writeln("		<nav>");
+document.writeln("    		<ul>");
+document.writeln("        		<li><a href=\"../cn/index.html\">主页</a></li>");
+document.writeln("        		<li><a href=\"talks.html\">学术报告</a></li>");
+document.writeln("        		<li><a href=\"teaching.html\">教学</a></li>");
+document.writeln("        		<li><a href=\"doc.html\">文档</a></li>");
+document.writeln("        		<li><a href=\"starcharts.html\">星图</a></li>");
+document.writeln("        		<li><a href=\"links.html\">链接</a></li>");
+document.writeln("    		</ul>");
+document.writeln("		</nav>");
